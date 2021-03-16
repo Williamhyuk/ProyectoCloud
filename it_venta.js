3b16373a96a01bde606be6e2f00b25945d170c5f
syomirachambi <71735587+syomirachambi@users.noreply.github.com>
2021-03-16 18:39:38 -0500

    ubicacion e imagenes

--- it_venta.js
+++ it_venta.js
@@ -14,22 +14,22 @@ let id = '';
  * @param {string} cant
  */
 const saveTask = (title, description, price, cant) =>
-  db.collection("tasks").doc().set({
+  db.collection("libros").doc().set({
     title,
     description,
     price,
     cant,
   });
 
-const getTasks = () => db.collection("tasks").get();
+const getTasks = () => db.collection("libros").get();
 
-const onGetTasks = (callback) => db.collection("tasks").onSnapshot(callback);
+const onGetTasks = (callback) => db.collection("libros").onSnapshot(callback);
 
-const deleteTask = (id) => db.collection("tasks").doc(id).delete();
+const deleteTask = (id) => db.collection("libros").doc(id).delete();
 
-const getTask = (id) => db.collection("tasks").doc(id).get();
+const getTask = (id) => db.collection("libros").doc(id).get();
 
-const updateTask = (id, updatedTask) => db.collection('tasks').doc(id).update(updatedTask);
+const updateTask = (id, updatedTask) => db.collection('libros').doc(id).update(updatedTask);
 
 window.addEventListener("DOMContentLoaded", async (e) => {
   onGetTasks((querySnapshot) => {
@@ -42,6 +42,19 @@ window.addEventListener("DOMContentLoaded", async (e) => {
     <p>${task.description}</p>
     <p> Precio: ${task.price}</p>
     <p> Stock: ${task.cant}</p>
+    <div>
+      <style>img{height: 200px; width: 200px; border: 2px black;}</style> 
+      <img id="myimg"> <label id="UpProgress"></label> <br> <br>
+    </div>
+
+
+    <script id="MainScript">
+    
+    
+
+    </script>
+
+
     <div>
       <button class="btn btn-primary btn-delete" data-id="${doc.id}">
         🗑 Eliminar
@@ -90,149 +103,34 @@ window.addEventListener("DOMContentLoaded", async (e) => {
 
 taskForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-
   const title = taskForm["task-title"];
   const description = taskForm["task-description"];
   const price = taskForm["task-price"];
   const cant = taskForm["task-cant"];
-
-  try {
-    if (!editStatus) {
-      await saveTask(title.value, description.value, price.value, cant.value);
-    } else {
-      await updateTask(id, {
-        title: title.value,
-        description: description.value,
-        price: price.value,
-        cant: cant.value
-      })
-
-      editStatus = false;
-      id = '';
-      taskForm['btn-task-form'].innerText = 'Publicar';
-    }
-
-    taskForm.reset();
-    title.focus();
-  } catch (error) {
+  if (title == null && description == null && price == null && cant == null){
     console.log(error);
   }
-});
-const taskForm = document.getElementById("task-form");
-const tasksContainer = document.getElementById("tasks-container");
+  else{
+    try {
+      if (!editStatus) {
+        await saveTask(title.value, description.value, price.value, cant.value);
+      } else {
+        await updateTask(id, {
+          title: title.value,
+          description: description.value,
+          price: price.value,
+          cant: cant.value
+        })
 
-let editStatus = false;
-let id = '';
+        editStatus = false;
+        id = '';
+        taskForm['btn-task-form'].innerText = 'Publicar';
+      }
 
-/**
- * Save a New Task in Firestore
- * @param {string} title the title of the Task
- * @param {string} description the description of the Task
- * @param {string} price
- * @param {string} cant
- */
-const saveTask = (title, description, price, cant) =>
-  db.collection("tasks").doc().set({
-    title,
-    description,
-    price,
-    cant,
-  });
-
-const getTasks = () => db.collection("tasks").get();
-
-const onGetTasks = (callback) => db.collection("tasks").onSnapshot(callback);
-
-const deleteTask = (id) => db.collection("tasks").doc(id).delete();
-
-const getTask = (id) => db.collection("tasks").doc(id).get();
-
-const updateTask = (id, updatedTask) => db.collection('tasks').doc(id).update(updatedTask);
-
-window.addEventListener("DOMContentLoaded", async (e) => {
-  onGetTasks((querySnapshot) => {
-    tasksContainer.innerHTML = "";
-    querySnapshot.forEach((doc) => {
-      const task = doc.data();
-
-      tasksContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
-    <h3 class="h5">${task.title}</h3>
-    <p>${task.description}</p>
-    <p> Precio: ${task.price}</p>
-    <p> Stock: ${task.cant}</p>
-    <div>
-      <button class="btn btn-primary btn-delete" data-id="${doc.id}">
-        🗑 Eliminar
-      </button>
-      <button class="btn btn-secondary btn-edit" data-id="${doc.id}">
-        🖉 Editar
-      </button>
-    </div>
-  </div>`;
-    });
-
-    const btnsDelete = tasksContainer.querySelectorAll(".btn-delete");
-    btnsDelete.forEach((btn) =>
-      btn.addEventListener("click", async (e) => {
-        console.log(e.target.dataset.id);
-        try {
-          await deleteTask(e.target.dataset.id);
-        } catch (error) {
-          console.log(error);
-        }
-      })
-    );
-
-    const btnsEdit = tasksContainer.querySelectorAll(".btn-edit");
-    btnsEdit.forEach((btn) => {
-      btn.addEventListener("click", async (e) => {
-        try {
-          const doc = await getTask(e.target.dataset.id);
-          const task = doc.data();
-          taskForm["task-title"].value = task.title;
-          taskForm["task-description"].value = task.description;
-          taskForm["task-price"].value = task.price;
-          taskForm["task-cant"].value = task.cant;
-
-          editStatus = true;
-          id = doc.id;
-          taskForm["btn-task-form"].innerText = "Actualizar";
-
-        } catch (error) {
-          console.log(error);
-        }
-      });
-    });
-  });
-});
-
-taskForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const title = taskForm["task-title"];
-  const description = taskForm["task-description"];
-  const price = taskForm["task-price"];
-  const cant = taskForm["task-cant"];
-
-  try {
-    if (!editStatus) {
-      await saveTask(title.value, description.value, price.value, cant.value);
-    } else {
-      await updateTask(id, {
-        title: title.value,
-        description: description.value,
-        price: price.value,
-        cant: cant.value
-      })
-
-      editStatus = false;
-      id = '';
-      taskForm['btn-task-form'].innerText = 'Publicar';
+      taskForm.reset();
+      title.focus();
+    } catch (error) {
+      console.log(error);
     }
-
-    taskForm.reset();
-    title.focus();
-  } catch (error) {
-    console.log(error);
   }
 });
